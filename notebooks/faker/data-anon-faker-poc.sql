48a74c6925db0835d0fe04311e1d7f550291b1eb
Alex Rolls <arolls99@gmail.com>
2020-04-01 15:59:06 +0100

    Update to faker notebook

--- notebooks/faker/data-anon-faker-poc.sql
+++ notebooks/faker/data-anon-faker-poc.sql
@@ -4,18 +4,6 @@
 
 -- COMMAND ----------
 
-DROP TABLE IF EXISTS datatable;
-
-CREATE TABLE datatable
-USING csv
-OPTIONS (path "/FileStore/tables/uk_500-626ee.csv", header "true")
-
--- COMMAND ----------
-
-SELECT * from datatable LIMIT 5
-
--- COMMAND ----------
-
 -- MAGIC %sh pip install Faker unicodecsv
 
 -- COMMAND ----------
@@ -23,32 +11,46 @@ SELECT * from datatable LIMIT 5
 -- MAGIC %python
 -- MAGIC import csv
 -- MAGIC from faker import Factory
+-- MAGIC from faker.providers import *
 -- MAGIC from collections import defaultdict
 -- MAGIC 
 -- MAGIC def anonymize_rows(rows):
--- MAGIC     """
--- MAGIC     Rows is an iterable of dictionaries that contain name and
--- MAGIC     email fields that need to be anonymized.
--- MAGIC     """
+-- MAGIC     # Rows is an iterable of dictionaries that contain name and email fields that need to be anonymized.
 -- MAGIC     # Load faker
--- MAGIC     faker  = Factory.create()
+-- MAGIC     f = Factory.create('en_GB')
 -- MAGIC 
 -- MAGIC     # Create mappings of rows to faked data.
--- MAGIC     names = defaultdict(faker.first_name)
+-- MAGIC     first_names    = defaultdict(f.first_name)
+-- MAGIC     last_names     = defaultdict(f.last_name)
+-- MAGIC     company_names  = defaultdict(f.company)
+-- MAGIC     house_number   = defaultdict(f.building_number)
+-- MAGIC     street_name    = defaultdict(f.street_name)
+-- MAGIC     cities         = defaultdict(f.city)
+-- MAGIC     counties       = defaultdict(f.city_suffix)
+-- MAGIC     postals        = defaultdict(f.postcode)
+-- MAGIC     phones1        = defaultdict(f.msisdn)
+-- MAGIC     phones2        = defaultdict(f.phone_number)
+-- MAGIC     emails         = defaultdict(f.email)
 -- MAGIC 
 -- MAGIC     # Iterate over the rows from the file and yield anonymized rows.
 -- MAGIC     for row in rows:
 -- MAGIC         # Replace first_name with faked fields.
--- MAGIC         row["first_name"] = names[row["first_name"]]
+-- MAGIC         row["first_name"]   = first_names[row["first_name"]]
+-- MAGIC         row["last_name"]    = last_names[row["last_name"]]
+-- MAGIC         row["company_name"] = company_names[row["company_name"]]
+-- MAGIC         row["address"]      = house_number[row["address"]] + " " + street_name[row["address"]]
+-- MAGIC         row["city"]         = cities[row["city"]]
+-- MAGIC         row["county"]       = counties[row["county"]]
+-- MAGIC         row["postal"]       = postals[row["postal"]]
+-- MAGIC         row["phone1"]       = phones1[row["phone1"]]
+-- MAGIC         row["phone2"]       = phones2[row["phone2"]]
+-- MAGIC         row["email"]        = emails[row["email"]]
 -- MAGIC 
 -- MAGIC         # Yield the row back to the caller
 -- MAGIC         yield row
 -- MAGIC 
 -- MAGIC def anonymize(source, target):
--- MAGIC     """
--- MAGIC     The source argument is a path to a CSV file containing data to anonymize,
--- MAGIC     while target is a path to write the anonymized CSV data to.
--- MAGIC     """
+-- MAGIC     #The source argument is a path to a CSV file containing data to anonymize, while target is a path to write the anonymized CSV data to.
 -- MAGIC     with open(source, newline='') as csvin:
 -- MAGIC         with open(target, 'w') as csvout:
 -- MAGIC             # Use the DictReader to easily extract fields
@@ -63,18 +65,48 @@ SELECT * from datatable LIMIT 5
 
 -- COMMAND ----------
 
-DROP TABLE IF EXISTS dataanontable;
+DROP TABLE IF EXISTS data_table;
 
-CREATE TABLE dataanontable
-USING csv
-OPTIONS (path "/FileStore/tables/uk_500-626ee-anonymized.csv", header "true")
+CREATE TABLE `data_table` (`first_name` STRING, `last_name` STRING, `company_name` STRING, `address` STRING, `city` STRING, `county` STRING, `postal` STRING, `phone1` STRING, `phone2` STRING, `email` STRING, `web` STRING)
+USING com.databricks.spark.csv
+OPTIONS (
+  `multiLine` 'false',
+  `escape` '"',
+  `header` 'true',
+  `delimiter` ',',
+  path 'dbfs:/FileStore/tables/uk_500-626ee.csv'
+);
+
+DROP TABLE IF EXISTS data_table_anon;
+
+CREATE TABLE `data_table_anon` (`first_name` STRING, `last_name` STRING, `company_name` STRING, `address` STRING, `city` STRING, `county` STRING, `postal` STRING, `phone1` STRING, `phone2` STRING, `email` STRING, `web` STRING)
+USING com.databricks.spark.csv
+OPTIONS (
+  `multiLine` 'false',
+  `escape` '"',
+  `header` 'true',
+  `delimiter` ',',
+  path 'dbfs:/FileStore/tables/uk_500-626ee-anonymized.csv'
+);
 
 -- COMMAND ----------
 
-SELECT * from dataanontable LIMIT 5
+SELECT * from data_table ORDER BY web LIMIT 5;
+
+-- COMMAND ----------
+
+SELECT * from data_table_anon ORDER BY web LIMIT 5;
 
 -- COMMAND ----------
 
 -- MAGIC %python
 -- MAGIC dbutils.fs.rm("dbfs:/FileStore/tables/uk_500-626ee-anonymized.csv")
 -- MAGIC display(dbutils.fs.ls("dbfs:/FileStore/tables/"))
+
+-- COMMAND ----------
+
+DROP TABLE data_table
+
+-- COMMAND ----------
+
+DROP TABLE data_table_anon
